8f3de84fa2497aebd4ac40db9b5bc2691fe9d067
mohankhobarkhede7-dev <mohankhobarkhede7@gmail.com>
2025-10-09 13:58:20 +0530

    Create Assignment1.py

---
+++ Assignment1.py
@@ -0,0 +1,8 @@
+a=input("Enter First number\n:",)
+b=input("Enter second number\n:",)
+a=int(a)
+b=int(b)
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
